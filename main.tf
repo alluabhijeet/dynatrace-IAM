2b15fa9b96a417c639510d00c524e78ba97d6f2a
alluabhijeet <120827058+alluabhijeet@users.noreply.github.com>
2024-03-05 12:47:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,3 +46,27 @@ resource "dynatrace_iam_policy" "platform_policy" {
   environment     = var.environment
   statement_query = "${var.platform_teams_permissions.globalPermissions}; ${var.platform_teams_permissions.ManagementZonePermissions} WHERE environment:management-zone IN (\"${join("\", \"", each.value.ManagementZone)}\");"
 }
+
+resource "dynatrace_iam_group" "product_group" {
+  for_each = { for team in var.product_teams : team.name => team }
+  name     = each.key
+}
+
+resource "dynatrace_iam_group" "platform_group" {
+  for_each = { for team in var.platform_teams : team.name => team }
+  name     = each.key
+}
+
+resource "dynatrace_iam_policy_bindings" "product_bindings" {
+  for_each    = { for team in var.product_teams : team.name => team }
+  group       = dynatrace_iam_group.product_group[each.key].id
+  environment = var.environment
+  policies    = [dynatrace_iam_policy.product_policy[each.key].id]
+}
+
+resource "dynatrace_iam_policy_bindings" "platform_bindings" {
+  for_each    = { for team in var.platform_teams : team.name => team }
+  group       = dynatrace_iam_group.platform_group[each.key].id
+  environment = var.environment
+  policies    = [dynatrace_iam_policy.platform_policy[each.key].id]
+}
